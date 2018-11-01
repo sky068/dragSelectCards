23e6cfb841afea5d071f0c121e56df35ef5ad208
SkyXU <skyxu@tcl.com>
2018-11-01 18:34:44 +0800

    add demo.gif

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hello-world
 Hello world new project template.
+![](./demo.gif)
--- library/imports/a9/a997f41c-ff85-40a8-9ecf-37d091f1e33a.js
+++ library/imports/a9/a997f41c-ff85-40a8-9ecf-37d091f1e33a.js
@@ -4,15 +4,11 @@ cc._RF.push(module, 'a997fQc/4VAqJ7PN9CR8eM6', 'CardCtrl');
 
 "use strict";
 
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+/**
+ * Created by skyxu on 2018/11/1.
+ * 
+ * 卡牌组件
+ */
 
 cc.Class({
     extends: cc.Component,
